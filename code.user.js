bd49deedc2bbff1d09d4e1907f4361bd3f0162b5
FREDERICK <github@FREDERICK.win>
2021-09-02 19:43:02 +0200

    Restructure information retrieving

--- code.user.js
+++ code.user.js
@@ -14,21 +14,68 @@
 (function() {
     'use strict';
 
-    const RLShouldBe = 2000;
-    const LLShouldBe = 1000;
-    const HLShouldBe = 500;
-    const GCShouldBe = 50;
-    const PTShouldBe = 25;
-    const ICShouldBe = 100;
-    const AMShouldBe = 50;
+    const currentStage = 3;
+    const defence = [
+        {
+            name: 'rocketLauncher',
+            stage_1: 500,
+            stage_2: 1000,
+            stage_3: 2000,
+        },
+        {
+            name: 'laserCannonLight',
+            stage_1: 200,
+            stage_2: 500,
+            stage_3: 1000,
+        },
+        {
+            name: 'laserCannonHeavy',
+            stage_1: 100,
+            stage_2: 200,
+            stage_3: 500,
+        },
+        {
+            name: 'gaussCannon',
+            stage_1: 10,
+            stage_2: 20,
+            stage_3: 50,
+        },
+        {
+            name: 'plasmaCannon',
+            stage_1: 5,
+            stage_2: 10,
+            stage_3: 25,
+        },
+        {
+            name: 'ionCannon',
+            stage_1: 10,
+            stage_2: 50,
+            stage_3: 100,
+        },
+        {
+            name: 'missileInterceptor',
+            stage_1: 10,
+            stage_2: 20,
+            stage_3: 50,
+        }
+    ];
+    const facilities = [
+        {
+            name: 'roboticsFactory',
+            value: 8,
+        },
+        {
+            name: 'shipyard',
+            value: 8,
+        },
+        {
+            name: 'missileSilo',
+            value: 3,
+        }
+    ];
 
-    setShouldBeText('rocketLauncher', RLShouldBe);
-    setShouldBeText('laserCannonLight', LLShouldBe);
-    setShouldBeText('laserCannonHeavy', HLShouldBe);
-    setShouldBeText('gaussCannon', GCShouldBe);
-    setShouldBeText('plasmaCannon', PTShouldBe);
-    setShouldBeText('ionCannon', ICShouldBe);
-    setShouldBeText('missileInterceptor', AMShouldBe);
+    defence.forEach(def => setShouldBeText(def.name, def[`stage_${currentStage}`]));
+    facilities.forEach(fac => setShouldBeText(fac.name, fac.value));
 
     function setShouldBeText(className, value) {
         const shouldBeStyling = 'style="color:#6f9fc8;bottom:5px;left:15px;position:absolute;z-index:99;"';
