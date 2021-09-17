a7fd7649c4fdc7199d6141eec0f1047c31ef648b
FREDERICK <github@FREDERICK.win>
2021-09-17 14:32:42 +0200

    Bump version

--- code.user.js
+++ code.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OGame Helper
 // @namespace    https://github.com/MrBurrBurr
-// @version      0.8
+// @version      0.9
 // @description  see you in space, cowboys!
 // @author       FREDERICK
 // @match        https://*.ogame.gameforge.com/*
